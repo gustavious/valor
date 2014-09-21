e58e5006af1aea30cd5e71dad2104ac470704b20
gustavious <gusalegria18@gmail.com>
2014-09-21 12:31:37 -0500

    l

--- src/co/edu/uniandes/valorAndes/vos/OperacionValue.java
+++ src/co/edu/uniandes/valorAndes/vos/OperacionValue.java
@@ -1,6 +1,17 @@
 package co.edu.uniandes.valorAndes.vos;
 
+import java.util.Date;
+
 public class OperacionValue
 {
 
+	
+	private int cantidad;
+	private Date fechaOrden;
+	private String tipoCompraVenta;
+	public ValorValue valor;
+
+	public OperacionValue(){
+
+	}
 }
