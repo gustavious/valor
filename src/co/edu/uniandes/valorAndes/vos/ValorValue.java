e3f5de85a8eb680d519dbb8feaec940650f0a828
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2014-09-21 13:06:25 -0500

    Cambio en valorValue---getters and setters

--- src/co/edu/uniandes/valorAndes/vos/ValorValue.java
+++ src/co/edu/uniandes/valorAndes/vos/ValorValue.java
@@ -1,18 +1,86 @@
 package co.edu.uniandes.valorAndes.vos;
 
+import java.sql.Date;
+
 public class ValorValue 
 {
 	//Atributos
 	
 	public String nombre;
 	
-	public String fechaCreacion;
+	public Date fechaExpiracion;
 	
 	public int id;
 	
 	public double valor;
 	
 	public int idUsuario;
+	
+	public String tipo;
+	
+	public String negociado;
+	
+	public ValorValue()
+	{
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Date getFechaExpiracion() {
+		return fechaExpiracion;
+	}
+
+	public void setFechaExpiracion(Date fechaExpiracion) {
+		this.fechaExpiracion = fechaExpiracion;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNegociado() {
+		return negociado;
+	}
+
+	public void setNegociado(String negociado) {
+		this.negociado = negociado;
+	}
+	
 	
 	
 }
